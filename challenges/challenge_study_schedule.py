031c48f08376a27ee203c28323dae7eff818ecb4
Thiago Souza <78744052+thisouzadev@users.noreply.github.com>
2022-04-29 10:12:12 -0300

    feat: study schedule

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,2 +1,11 @@
 def study_schedule(permanence_period, target_time):
-    """ Faça o código aqui. """
+    count = 0
+
+    for start_time, end_time in permanence_period:
+        try:
+            if start_time <= target_time <= end_time:
+                count += 1
+        except TypeError:
+            return None
+
+    return count
